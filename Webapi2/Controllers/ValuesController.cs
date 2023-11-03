33310bcc8563892b4124e1688c65653b38b897af
Asus <Asus@DESKTOP-KU0AVPO>
2023-11-03 22:51:21 +0330

    Add project files.

---
+++ Webapi2/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webapi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+     
+        [HttpGet]
+        public IActionResult Get(int x1, int y1, int x2, int y2, int x3, int y3)
+        {
+            bool areCollinear = (y3 - y2) * (x2 - x1) == (y2 - y1) * (x3 - x2);
+            return Ok(new { x1, y1, x2, y2, x3, y3, areCollinear });
+        }
+    }
+}
+    
---
+++ Webapi2/Controllers/usersController1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Webapi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usersController1 : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Amirabbas", "Ali" };
+        }
+
+        // GET api/<usersController1>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"value {id}";
+        }
+
+        // POST api/<usersController1>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<usersController1>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<usersController1>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
